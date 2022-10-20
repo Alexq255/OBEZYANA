7b86e763fd4e14b1b2ea38795e945cb67b8a53ff
Alexq255 <92524226+Alexq255@users.noreply.github.com>
2022-10-20 12:51:57 +0300

    Changed

---
+++ app/src/main/java/com/example/obezyana/Load.java
@@ -0,0 +1,14 @@
+package com.example.obezyana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Load extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+    }
+}
